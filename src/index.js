9e856f9a3e35115fd549caa8dfb1163c569c2940
Ansar Ahmad <ahmadansar264@gmail.com>
2025-06-17 10:18:56 +0500

    Utils Setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({ path: '../.env' });
 
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT,() => {
+        console.log(`Server has Started at Port: ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log(console.log("MongoDB Connection Failed", err))
+})
 
 // node -r dotenv/config "D:\Learning & Skill\MERN STACK\BackEnd\MERN-Youtube-with-Twitter\node_modules\nodemon\bin\nodemon.js" src/index.js
